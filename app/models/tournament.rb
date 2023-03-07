e3301a86dc09f669615230b6a29b5dc1bf3362af
sofia flores <saflores2@uc.cl>
2023-03-07 00:50:09 -0300

    agrego precio a torneos

--- app/models/tournament.rb
+++ app/models/tournament.rb
@@ -1,5 +1,4 @@
 class Tournament < ApplicationRecord
   belongs_to :user
-  belongs_to :ubication
   has_many :participations, dependent: :destroy
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_07_023310) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_07_034845) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -76,6 +76,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_07_023310) do
     t.string "ubication_address"
     t.float "lat"
     t.float "lng"
+    t.integer "price"
     t.index ["user_id"], name: "index_tournaments_on_user_id"
   end
 
