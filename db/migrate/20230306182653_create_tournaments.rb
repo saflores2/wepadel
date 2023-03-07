bf613d45dc0602339fe26d58fb08a9bce18dc4fe
Fabian Rubio <fabian.rubiocs@gmail.com>
2023-03-07 14:54:02 -0300

    add acciones modelo participation

--- db/migrate/20230306182653_create_tournaments.rb
+++ db/migrate/20230306182653_create_tournaments.rb
@@ -2,7 +2,6 @@ class CreateTournaments < ActiveRecord::Migration[7.0]
   def change
     create_table :tournaments do |t|
       t.references :user, null: false, foreign_key: true
-      t.references :ubication, null: false, foreign_key: true
       t.string :name
       t.datetime :start_date
       t.datetime :end_date
