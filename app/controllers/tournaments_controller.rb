b1278d35fe9b79967d663a13cbd7044921eaf114
sofia flores <saflores2@uc.cl>
2023-03-08 17:30:24 -0300

    cambios en participations controller

--- app/controllers/tournaments_controller.rb
+++ app/controllers/tournaments_controller.rb
@@ -19,6 +19,7 @@ class TournamentsController < ApplicationController
 
   def show
     authorize @tournament
+    @participation = Participation.new
   end
 
   def new
---
+++ app/policies/participation_policy.rb
@@ -0,0 +1,5 @@
+class ParticipationPolicy < ApplicationPolicy
+  def create?
+    true
+  end
+end
