454eccaebd766079cbda91cce956df40881ec9c7
steve brusa <stevebrusagerald@gmail.com>
2023-03-10 16:10:06 -0300

    formulario

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,6 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.0].define(version: 2023_03_08_225344) do
-
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -62,11 +61,11 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_08_225344) do
   create_table "participations", force: :cascade do |t|
     t.bigint "tournament_id", null: false
     t.bigint "user_id", null: false
-    t.bigint "partner_id"
     t.string "partner_email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "status"
+    t.bigint "partner_id"
     t.index ["partner_id"], name: "index_participations_on_partner_id"
     t.index ["tournament_id"], name: "index_participations_on_tournament_id"
     t.index ["user_id"], name: "index_participations_on_user_id"
@@ -119,6 +118,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_08_225344) do
   add_foreign_key "messages", "users"
   add_foreign_key "participations", "tournaments"
   add_foreign_key "participations", "users"
-  add_foreign_key "participations", "users", column: "partner_id"
   add_foreign_key "tournaments", "users"
 end
