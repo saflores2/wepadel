1674a1d9b3f8cbb7f267f04f0e77bc552e73bbd9
steve brusa <stevebrusagerald@gmail.com>
2023-03-14 12:54:44 -0300

    arreglos homepage

--- app/javascript/controllers/prueba_controller.js
+++ app/javascript/controllers/prueba_controller.js
@@ -1,8 +1,18 @@
 import { Controller } from "@hotwired/stimulus"
+import Typed from "typed.js"
 
 // Connects to data-controller="prueba"
 export default class extends Controller {
   connect() {
     console.log("estoy conectado al controlador")
+    new Typed(this.element, {
+      strings: ["Encuentra los Mejores Torneos de Padel"],
+      typeSpeed: 50,
+      loop: true
+    })
+    new Typed(this.element, {
+      strings: ["We Padel"],
+      typeSpeed: 40,
+      loop: true
   }
 }
